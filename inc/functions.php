731b093da204c547e243495d84c5ddb2644b52da
Manjit Gurcha <mgurcha@csumb.edu>
2018-02-18 22:20:53 +0000

    Updated CSS file and added array functions

--- inc/functions.php
+++ inc/functions.php
@@ -10,32 +10,44 @@
                 "<img src='img/4die.png'/>",
                 "<img src='img/5die.png'/>",
                 "<img src='img/6die.png'/>");
+                
             
             asort($dicearray);
             
             $amount = count($dicearray)-1;
             echo "<h2 id='desc'>" . "There are a total of 4 dice displayed below, each with " . $amount . " sides. " . 
             "You will compete against the computer to see who wins this game!" . "</h2>";
+            
                 
             for($i=1; $i<5; $i++){
                 ${"roll" . $i } = rand(1,6);
                 
             }
+            
             print "<div id='format'>";
             for($i=1; $i<2; $i++){
                 echo $dicearray[$roll1];
                 echo $dicearray[$roll2];
-                $playerTotal = $roll1 + $roll2;
-                echo "<h2 id='h_score'>" . "Your total score is: " . $playerTotal . "</h2>";
+                $sum = array($roll1, $roll2);
+                $playerTotal = array_sum($sum);
+                $prod = array_product($sum);
+                echo "<h2 id = 'total_score_h'>" . "Your total score is: " . "</h2>". "<h2 id = 'h_score'>" . $playerTotal . "</h2>";
+                echo "<h2 id = 'product_h'>" . "The product of the two dice is: ". "</h2>" . "<h2 id = 'h_prod'>" . $prod . "</h2>";
             }
             print "</div>";
+
+            
+            print "<br>";
             
             print "<div id='second_format'>";
             for($i=1; $i<2; $i++){
                 echo $dicearray[$roll3];
                 echo $dicearray[$roll4];
-                $computerTotal= $roll3 + $roll4;
-                echo "<h2 id='c_score'>" . "Computer's total score is: " . $computerTotal . "</h2>";
+                $sum = array($roll3, $roll4);
+                $computerTotal= array_sum($sum);
+                $prod = array_product($sum);
+                echo "<h2 id = 'total_score_c'>" . "Computer's total score is: " . "</h2>" . "<h2 id = 'c_score'>" . $computerTotal . "</h2>";
+                echo "<h2 id = 'product_c'>" . "The product of the two dice is: ". "</h2>" . "<h2 id = 'c_prod'>" . $prod . "</h2>";
             }
             print "</div>";
         
@@ -52,31 +64,5 @@
                 print "Tie!";
             }
             print "</div>";
-           
-           /*
-            $roll1 = rand(1,6);
-            $roll2 = rand(1,6);
-            $roll3 = rand(1,6);
-            $roll4 = rand(1,6);
-            
-            $playerTotal = $roll1 + $roll2;
-            $computerTotal = $roll3 + $roll4;
-            
-            
-            
-            
-            print "<div>";
-            print $dicearray[$roll1];
-            print $dicearray[$roll2];
-            print "</div>";
-            
-            print "<div>";
-            print $dicearray[$roll3];
-            print $dicearray[$roll4];
-            print "</div>";
-            
-            echo "<h2>" . "Your total score is: " . $playerTotal . "</h2>";
-            echo "<h2>" . "The computer's total score is: " . $computerTotal . "</h2>";
-            */
-        ?>
+?>
         
